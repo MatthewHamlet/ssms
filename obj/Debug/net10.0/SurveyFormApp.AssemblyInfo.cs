d136e96a6fd285cbdae0e0145c99421674383fe2
miawmiaw-miaw <mattsim167@gmail.com>
2026-08-24 15:07:14 +0700

    design

--- obj/Debug/net10.0/SurveyFormApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/SurveyFormApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveyFormApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36da81de3378f86c0d8e9ab163dc1cc3d2e2aa62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853943e3bd863cc6a2f96660da6aed6cf9ddeccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveyFormApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurveyFormApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
