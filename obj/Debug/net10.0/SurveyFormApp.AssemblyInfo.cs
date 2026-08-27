98d87bcc231656a472d536df58f07dd6c04cb0d3
miawmiaw-miaw <mattsim167@gmail.com>
2026-08-27 15:28:53 +0700

    changed the position of kembali ke jawaban

--- obj/Debug/net10.0/SurveyFormApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/SurveyFormApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveyFormApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+326c0298a93d00b5603a668e41a1c1eacc5d3620")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e04749c1f784cee4b220628c3e016ff8b274cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveyFormApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurveyFormApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
